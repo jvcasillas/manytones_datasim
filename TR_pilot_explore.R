8d90a32e32d7fad00286b52b584368330360a3a5
Timo Roettger <timo.b.roettger@gmail.com>
2025-05-26 20:29:44 +0200

    plotting raw values

--- TR_pilot_explore.R
+++ TR_pilot_explore.R
@@ -7,7 +7,7 @@
 # Nifty code using the pacman package
 # it checks if the packages specified below are installed, if not, they will be installed, if yes, they will be loaded
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(brms, ggplot2, tidybayes, tidyverse, dplyr, truncnorm, lme4, faux, scam)
+pacman::p_load(brms, ggplot2, tidybayes, viridis, tidyverse, dplyr, truncnorm, lme4, faux, scam)
 
 # Set the current working directory to the one where this file is
 current_working_dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
@@ -27,7 +27,8 @@ xdata <- xdata |>
          # make response_f_rel relative to baseline 150hz      
          response_f_rel = response_f - 150,
          # relative to target
-         response_f_prop = response_f_rel / delta_f
+         response_f_prop = response_f_rel / delta_f,
+         presented_f = 150+delta_f_raw
          )
 
 
@@ -35,40 +36,43 @@ xdata <- xdata |>
 
 xdata_agg <- xdata |> 
   group_by(delta_f_raw, delta_t, participant, stimuli_type) |> 
-  summarise(response_f_prop = mean(response_f_prop, na.rm = TRUE))
+  summarise(response_f = mean(response_f, na.rm = TRUE))
   
 
 # Plot perceived f0 relative to presented f0
 ggplot(data = xdata, 
        aes(x = delta_t,
-           y = response_f_prop,
+           y = response_f,
            color = delta_f_raw)) + 
   geom_jitter(data = xdata_agg,
-              width = 0.1,
-              height = 0.1,
+              width = 1,
+              height = 1,
               alpha = 0.2) +
   facet_grid(. ~ delta_f_raw) + 
   # add a quick and dirty monotonic spline smooth (increasing)
-  geom_smooth(data = xdata |> filter(delta_f_raw > 0),
-              method = "scam",
+  geom_smooth(data = xdata ,
+              method = "gam",
               lwd = 2,
-              formula = y ~ s(x, k = 5, bs = "mpi")) +
-  geom_smooth(data = xdata |> filter(delta_f_raw < 0),
-              method = "scam",
-              lwd = 2,
-              formula = y ~ s(x, k = 5, bs = "mpd")) +
-  scale_y_continuous(limits = c(-0.5,1)) +
+              formula = y ~ s(x, k = 5)) +
+  # geom_smooth(data = xdata |> filter(delta_f_raw > 0),
+  #             method = "scam",
+  #             lwd = 2,
+  #             formula = y ~ s(x, k = 5, bs = "mpi")) +
+  # geom_smooth(data = xdata |> filter(delta_f_raw < 0),
+  #             method = "scam",
+  #             lwd = 2,
+  #             formula = y ~ s(x, k = 5, bs = "mpd")) +
   theme_minimal()
 
 
 ## Model
 # Trying a simple gam without constraints on shape
-priors_gam = c(prior(normal(0, 0.3), class = Intercept),
-               prior(normal(0, 0.3), class = b),
-               prior(student_t(3, 0, 1), class = sds),
-               prior(normal(0, 0.3), class = sigma))
+priors_gam = c(prior(normal(150, 20), class = Intercept),
+               prior(normal(0, 10), class = b),
+               prior(student_t(3, 0, 10), class = sds),
+               prior(normal(0, 10), class = sigma))
 
-xmdl_gam <- brm(response_f_prop ~ delta_f_raw + s(delta_t, k = 5, by = delta_f_raw),
+xmdl_gam <- brm(response_f ~ delta_f_raw + s(delta_t, k = 5, by = delta_f_raw),
                 prior = priors_gam,
                 chains = 4,
                 cores = 4,
@@ -82,7 +86,7 @@ xmdl_gam <- brm(response_f_prop ~ delta_f_raw + s(delta_t, k = 5, by = delta_f_r
 conditional_effects(xmdl_gam)
 
 # pp_check()
-pp_check(xmdl_gam) # not too bad
+pp_check(xmdl_gam) # not too bad - symmetric and unimodal
 
 # prepare model predictions
 new <- expand.grid(delta_t = seq(40,120,5),
@@ -95,6 +99,12 @@ new |>
 ggplot(aes(x = delta_t,
            y = Estimate,
            color = delta_f_raw)) + 
+  geom_hline(data = xdata,
+             yintercept = unique(xdata$presented_f),
+             col = c("#55AA84", "#79C66E", "#3F5187", "#FAD155",
+                     "#3F718B", "#432E77",  "#B5DB54", "#3F1250"),
+             lty = "dashed",
+             alpha = 0.5) +
   geom_ribbon(aes(ymin = Q2.5, 
                   ymax = Q97.5,
                   group = delta_f_raw),
@@ -102,13 +112,18 @@ ggplot(aes(x = delta_t,
               color = NA,
               alpha = 0.5) +
   geom_line(aes(group = delta_f_raw),
-            lwd = 2) +
-  scale_y_continuous(limits = c(-1,1)) +
+            lwd = 1) +
+
+  scale_color_viridis() +
+  scale_y_continuous(breaks = unique(xdata$presented_f)) +
   theme_minimal()
 
 # almost linear
 
 ## scam
+
+# run predict-mpi.r before trying to predict
+
 # using scam for monotonic splines?
 xmdl_mpi <- brm(response_f_prop ~ delta_f_raw + s(delta_t, bs = "mpi", k = 4, by = delta_f_raw),
                 prior = priors_gam,
@@ -122,10 +137,12 @@ xmdl_mpi <- brm(response_f_prop ~ delta_f_raw + s(delta_t, bs = "mpi", k = 4, by
   
 # prepare model predictions
 new_mpi <- expand.grid(delta_t = seq(40,120,5),
-                       delta_f = unique(xdata$delta_f_raw))
+                       delta_f_raw = unique(xdata$delta_f_raw))
 
 # breaks down here "Error in X %*% object$diagRP : non-conformable arguments"
-new_mpi <- cbind(new_mpi, fitted(xmdl_mpi, newdata = new))
+new_mpi <- cbind(new_mpi, fitted(xmdl_mpi, newdata = new_mpi))
+# breaks down here "Error in object$m + 1 : non-numeric argument to binary operator"
+new_mpi <- cbind(new_mpi, Predict.matrix.mpi.smooth(xmdl_mpi, data = new_mpi))
 
 # plot model predictions
 new_mpi |> 
@@ -142,3 +159,4 @@ new_mpi |>
             lwd = 2) +
   scale_y_continuous(limits = c(-1,1)) +
   theme_minimal()
+
